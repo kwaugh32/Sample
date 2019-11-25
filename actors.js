6f6c9904ae3215752446ef208232afbb0459a31c
Ken Waugh <kwaugh@givelify.com>
2019-11-25 14:47:55 -0500

    Create actors.js

---
+++ actors.js
@@ -0,0 +1,32 @@
+{
+  "Actors": [
+    {
+      "name": "Tom Cruise",
+      "age": 56,
+      "Born At": "Syracuse, NY",
+      "Birthdate": "July 3, 1962",
+      "photo": "https://imdb.com/img/tom-cruise.jpg"
+    },
+    {
+      "name": "Robert Downey Jr.",
+      "age": 53,
+      "Born At": "New York City, NY",
+      "Birthdate": "April 4, 1965",
+      "photo": "https://imdb.com/img/Robert-Downey-Jr.jpg"
+    },
+    {
+      "name": "Tom Hanks",
+      "age": 73,
+      "Born At": "Concord, CA",
+      "Birthdate": "July 9, 1946",
+      "photo": "https://imdb.com/img/Tom-Hanks.jpg"
+    },
+    {
+      "name": "Fozzie Bear",
+      "age": 43,
+      "Born At": "Concord, CA",
+      "Birthdate": "Sept 10, 1976",
+      "photo": "https://imdb.com/img/Fozzie-Bear.jpg"
+    }
+  ]
+}
